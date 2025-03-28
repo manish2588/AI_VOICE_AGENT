e1af6d12d8aa6889af27be80d57baf2853646c58
manish2588 <manishkc258@gmail.com>
2025-03-28 21:41:51 +0545

    Added

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Button } from '@/components/ui/button';
+import { UserButton } from '@stackframe/stack';
 import React from 'react'
 
 function page() {
   return (
     <div className='text-center'>Page
      <Button>Hello</Button>
+     <UserButton></UserButton>
     </div>
    
   )
